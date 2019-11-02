c75584f5b6be73174a723165896dd45f5451a874
Kim McCaskill <kimmccaskill92@gmail.com>
2019-11-01 18:06:39 -0600

    Add loaded greeting functionality

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var plyrOneInput = document.querySelector("#plyr-1-input");
 var playBtn = document.querySelector("#play-game-btn");
+var greeting = document.querySelector("article");
+var form = document.querySelector(".form-container");
 
 window.addEventListener('load', playDisableToggle);
 plyrOneInput.addEventListener('keyup', playDisableToggle);
+playBtn.addEventListener('click', loadGreeting);
 
 function playDisableToggle() {
   if (plyrOneInput.value) {
@@ -14,4 +17,9 @@ function playDisableToggle() {
     playBtn.style.color = '#7a7a7a';
     playBtn.style.cursor = 'not-allowed';
   }
+}
+
+function loadGreeting() {
+  greeting.style.display = "flex";
+  form.style.display = "none";
 }
