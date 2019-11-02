ed9c3270395741889c92b6bbf67c1a8d6e3e29b0
Kim McCaskill <kimmccaskill92@gmail.com>
2019-11-01 23:34:17 -0600

    Add interpolation of player name

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 var plyrOneInput = document.querySelector("#plyr-1-input");
+var plyrTwoInput = document.querySelector("#plyr-2-input");
 var playBtn = document.querySelector("#play-game-btn");
 var greeting = document.querySelector("article");
 var form = document.querySelector(".form-container");
+var playerOneName = document.querySelector(".plyr-one-name");
+var playerTwoName = document.querySelector(".plyr-two-name");
 
 window.addEventListener('load', playDisableToggle);
 plyrOneInput.addEventListener('keyup', playDisableToggle);
+plyrTwoInput.addEventListener('keyup', playDisableToggle);
 playBtn.addEventListener('click', loadGreeting);
 
+
 function playDisableToggle() {
-  if (plyrOneInput.value) {
+  if (plyrOneInput.value && plyrTwoInput.value) {
     playBtn.disabled = false;
     playBtn.style.color = '#000000';
     playBtn.style.cursor = 'pointer';
@@ -22,4 +27,9 @@ function playDisableToggle() {
 function loadGreeting() {
   greeting.style.display = "flex";
   form.style.display = "none";
+  insertName();
+}
+
+function insertName() {
+  playerOneName.innerText = `WELCOME ${plyrOneInput.value.toUpperCase()} AND ${plyrTwoInput.value.toUpperCase()}!`;
 }
